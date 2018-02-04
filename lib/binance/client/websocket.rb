6a58f9b7a0a38081dd695f66f143fe40cb9afebd
Dinesh Gadge <dinesh@dineshgadge.com>
2018-02-04 20:46:41 +0530

    changed stream_url method so that it does not barf on additional parameters like startTime

--- lib/binance/client/websocket.rb
+++ lib/binance/client/websocket.rb
@@ -176,16 +176,19 @@ module Binance
       # :type   - The String type the stream will listen to
       # :level    - The String level to use for the depth stream (optional)
       # :interval - The String interval to use for the kline stream (optional)
-      def stream_url(options: {symbol: "", type: "", level: '', interval: ''})
+      def stream_url(options)
+        default_options = {symbol: "", type: "", level: '', interval: ''}
+        options = default_options.merge(options)
         symbol = options[:symbol]
         type = options[:type]
-        level = options[:level]
-        interval = options[:interval]
+        level = options[:level] || ""
+        interval = options[:interval] || ""
         "#{symbol.downcase}@#{type}".tap do |url|
           url << level
           url << "_#{interval}" unless interval.empty?
         end
       end
+
 
       # Internal: Initialize and return a Faye::WebSocket::Client
       #
